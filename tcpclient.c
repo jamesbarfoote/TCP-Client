5eb2c2065ed63881560df141ff6d72dc8c2f6a14
James Barfoote <james.barfoote@gmail.com>
2015-09-15 09:18:49 +1200

    Uploading

---
+++ tcpclient.c
@@ -0,0 +1,61 @@
+// NWEN 243, Victoria University of Wellington. Author, Kris Bubendorfer.
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+
+#define bufsize 1024
+int socket(int domain, int type, int protocol);
+int connect(int sockfd, const struct sockaddr *serv_addr, socklen_t addrlen);
+int read(int sockfd, void *buf, int count);
+int write(int sockfd, const void *buf, int count);
+int close(int fd);
+
+int main(argc, argv) int argc; char *argv[];{
+  int sock, rval;
+  struct hostent *host;
+  struct sockaddr_in server;  // not a pointer.
+  char buf[bufsize];
+
+  if(argc != 4){
+    printf("usage:\ntcpclient hostname port string\n\n");
+    return(-1);
+  }
+
+  // look up hostname (server) using DNS
+
+  if ((host = gethostbyname(argv[1])) == 0) {
+    fprintf(stderr, "%s: unknown host\n", argv[1]);
+    return(-1);
+  }
+
+  // Set up fields for socket to point to host and port
+
+  bcopy(host->h_addr, &server.sin_addr, host->h_length);
+  server.sin_family = AF_INET;
+  server.sin_port = htons(atoi(argv[2]));
+
+  // Create socket
+  int sockfd = socket(AF_INET, SOCK_STREAM, 1);
+  
+
+  // connect (3-way handshake)
+  connect(sockfd,  (struct sockaddr *)&server, 1024);
+
+  // Copy the arg into buf so we can send it to the server
+
+  strncpy(buf, argv[3], bufsize);
+
+  // Send sentence to server
+  write(sockfd, buf, bufsize);
+
+  // read response from server
+  read(sockfd, buf, bufsize);
+
+  // print result to window
+
+  fprintf(stdout,"%s\n", buf);
+  close(sockfd);
+}
